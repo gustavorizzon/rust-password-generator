3762717c0622ff312da59ef3272c25fb9c8e522b
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2022-10-21 09:50:40 -0300

    feat: generate random password using ascii table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+use std::char;
+
 fn main() {
-    println!("Hello, world!");
+  let mut password = String::new();
+
+  while password.len() < 32 {
+    password.push(
+      char::from_u32(
+        rand::thread_rng().gen_range(33..=126)
+      ).unwrap()
+    )
+  }
+
+  println!("The generated password is: {password}")
 }
