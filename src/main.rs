b1c0035e14a1c4c6422a4e659a95109963b0c40f
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2022-10-21 13:10:32 -0300

    feat: using args to specify password length

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 use rand::Rng;
 use std::char;
+use std::env;
 
-fn main() {
+fn print_usage() {
+  println!("Invalid call!\nUsage: ./rust-password-generator <password_length>");
+}
+
+fn gen_password(length: u32) -> String {
   let mut password = String::new();
 
-  while password.len() < 32 {
+  for _ in 1..=length {
     password.push(
       char::from_u32(
         rand::thread_rng().gen_range(33..=126)
       ).unwrap()
     )
   }
+
+  return password;
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() <= 1 {
+    print_usage();
+    return;
+  }
+
+  let length: u32 = match args[1].parse() {
+    Ok(length) => length,
+    Err(_) => {
+      print_usage();
+      return;
+    }
+  };
+
+  let password = gen_password(length);
 
   println!("The generated password is: {password}")
 }
